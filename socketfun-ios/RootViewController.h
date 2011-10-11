4b9eb4d5c1a9ff4e23201c34391debc47f185285
Seb <sek@gobas.de>
2011-10-11 19:15:37 +0200

    init xcode proj

---
+++ socketfun-ios/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  socketfun-ios
+//
+//  Created by Sebastian Kruschwitz on 11.10.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
---
+++ socketfun-ios/socketfun_iosAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  socketfun_iosAppDelegate.h
+//  socketfun-ios
+//
+//  Created by Sebastian Kruschwitz on 11.10.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface socketfun_iosAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
